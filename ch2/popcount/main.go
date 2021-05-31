069c242ba02ce5a39284d56dfa1419783c5a7ce4
pomyslowynick <rafal.rosa@mycit.ie>
2021-05-31 13:57:39 +0100

    Add main function to test PopCount

--- ch2/popcount/main.go
+++ ch2/popcount/main.go
@@ -1,4 +1,10 @@
-package popcount
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 // pc[i] is the population count of i.
 var pc [256]byte
@@ -10,6 +16,7 @@ func init() {
 }
 
 // PopCount returns the population count (number of set bits) of x.
+// Exercise 2.3
 func PopCount(x uint64) int {
 	return int(pc[byte(x>>(0*8))] +
 		pc[byte(x>>(1*8))] +
@@ -19,6 +26,16 @@ func PopCount(x uint64) int {
 		pc[byte(x>>(5*8))] +
 		pc[byte(x>>(6*8))] +
 		pc[byte(x>>(7*8))])
+}
+
+func main() {
+	start := time.Now()
+	for i := 0; i < 1000000000; i++ {
+		PopCount(uint64(rand.Int63()))
+	}
+	t := time.Now()
+	elapsed := t.Sub(start)
+	fmt.Printf("Time it took: %v\n", elapsed)
 }
 
 //!-
