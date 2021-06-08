8df035c3b110ff431b12b60e27a95974c11c3004
pomyslowynick <rafal.rosa@mycit.ie>
2021-06-08 22:44:48 +0100

    Add complete code for 4.11, working but requires improvements

--- ch4/cli-github-issues/cli-github-issues.go
+++ ch4/cli-github-issues/cli-github-issues.go
@@ -4,16 +4,18 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"time"
 )
 
 var repoOwner = flag.String("o", " ", "the username of the repo owner")
-var issueAction = flag.String("a", " ", "the action to be performed (create, update, read, delete)")
+var issueAction = flag.String("a", " ", "the action to be performed (create, update, read, close)")
 var repoName = flag.String("n", " ", "the repository name")
 
 const APIURL = "https://api.github.com/repos/"
@@ -34,6 +36,11 @@ type Issue struct {
 	Body      string    // in Markdown format
 }
 
+type SendIssue struct {
+	Title string `json:"title,omitempty"`
+	Body  string `json:"body,omitempty"`
+	State string `json:"state,omitempty"`
+}
 type User struct {
 	Login   string
 	HTMLURL string `json:"html_url"`
@@ -55,7 +62,13 @@ func main() {
 
 	switch *issueAction {
 	case "create":
-		createIssue(*repoOwner, *repoName)
+		var err error
+		result, err = createIssue(*repoOwner, *repoName)
+		if err != nil {
+			fmt.Printf("Process has exited when trying to create the issue: %v", err)
+			os.Exit(1)
+		}
+
 	case "read":
 		var err error
 		result, err = readIssue(*repoOwner, *repoName)
@@ -63,31 +76,71 @@ func main() {
 			fmt.Printf("Process has exited when trying to read the issue: %v", err)
 			os.Exit(1)
 		}
-		fmt.Println(result.CreatedAt.Clock())
 	case "update":
-		updateIssue(*repoOwner, *repoName)
-	case "delete":
-		deleteIssue(*repoOwner, *repoName)
+		var err error
+		result, err = updateIssue(*repoOwner, *repoName)
+		if err != nil {
+			fmt.Printf("Process has exited when trying to update the issue: %v", err)
+			os.Exit(1)
+		}
+
+	case "close":
+		var err error
+		result, err = closeIssue(*repoOwner, *repoName)
+		if err != nil {
+			fmt.Printf("Process has exited when trying to update the issue: %v", err)
+			os.Exit(1)
+		}
 	}
 
 	fmt.Printf("#%-6d \nLogin: %9.9s \nTitle: %-55s \nBody:\n %s\n",
 		result.Number, result.User.Login, result.Title, result.Body)
+	// fmt.Printf("\nTitle: %-55s \n",
+	// 	result.Title)
 
 }
 
-func createIssue(repoOwner, repoName string) (IssuesSearchResult, error) {
+func createIssue(repoOwner, repoName string) (*Issue, error) {
+	// Question, is this a good practice to throw an error in the helper function,
+	// or should it be checked here? Could look up the way it's done in other projects.
+	bearerToken := "Bearer " + getBearerToken()
+
 	input := bufio.NewScanner(os.Stdin)
 	fmt.Printf("Please input the title of the issue you want to create: ")
 	input.Scan()
 	issueTitle := input.Text()
-	fmt.Println(issueTitle)
 
 	fmt.Printf("Please input the body of the issue you want to create: ")
 	input.Scan()
 	issueBody := input.Text()
-	fmt.Println(issueBody)
 
-	return IssuesSearchResult{}, nil
+	sendIssue := SendIssue{Title: issueTitle, Body: issueBody}
+	formattedUrl := APIURL + repoOwner + "/" + repoName + "/issues"
+
+	jsonStr, err := json.Marshal(sendIssue)
+	if err != nil {
+		log.Fatalf("JSON marshaling failed: %s", err)
+	}
+
+	req, err := http.NewRequest("POST", formattedUrl, bytes.NewBuffer(jsonStr))
+	req.Header.Add("Authorization", bearerToken)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error in response. \n[ERROR] -", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%d status code \n", resp.StatusCode)
+
+	var result *Issue
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	return result, nil
 }
 
 func readIssue(repoOwner, repoName string) (*Issue, error) {
@@ -117,10 +170,102 @@ func readIssue(repoOwner, repoName string) (*Issue, error) {
 	return &result, nil
 }
 
-func updateIssue(repoOwner, repoName string) (IssuesSearchResult, error) {
-	return IssuesSearchResult{}, nil
+func updateIssue(repoOwner, repoName string) (*Issue, error) {
+
+	bearerToken := "Bearer " + getBearerToken()
+
+	input := bufio.NewScanner(os.Stdin)
+
+	fmt.Printf("Please input the number of the issue you want to update: ")
+	input.Scan()
+	issueNumber := input.Text()
+
+	fmt.Printf("Please input the title of the issue you want to update: ")
+	input.Scan()
+	issueTitle := input.Text()
+
+	fmt.Printf("Please input the body of the issue you want to update: ")
+	input.Scan()
+	issueBody := input.Text()
+
+	sendIssue := SendIssue{Title: issueTitle, Body: issueBody}
+	formattedUrl := APIURL + repoOwner + "/" + repoName + "/issues/" + issueNumber
+
+	jsonStr, err := json.Marshal(sendIssue)
+	if err != nil {
+		log.Fatalf("JSON marshaling failed: %s", err)
+	}
+
+	req, err := http.NewRequest("POST", formattedUrl, bytes.NewBuffer(jsonStr))
+	req.Header.Add("Authorization", bearerToken)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error in response. \n[ERROR] -", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%d status code \n", resp.StatusCode)
+
+	var result *Issue
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	return result, nil
 }
 
-func deleteIssue(repoOwner, repoName string) (IssuesSearchResult, error) {
-	return IssuesSearchResult{}, nil
+func closeIssue(repoOwner, repoName string) (*Issue, error) {
+
+	bearerToken := "Bearer " + getBearerToken()
+
+	input := bufio.NewScanner(os.Stdin)
+
+	fmt.Printf("Please input the number of the issue you want to close: ")
+	input.Scan()
+	issueNumber := input.Text()
+
+	sendIssue := SendIssue{State: "closed"}
+	formattedUrl := APIURL + repoOwner + "/" + repoName + "/issues/" + issueNumber
+
+	jsonStr, err := json.Marshal(sendIssue)
+	if err != nil {
+		log.Fatalf("JSON marshaling failed: %s", err)
+	}
+
+	req, err := http.NewRequest("POST", formattedUrl, bytes.NewBuffer(jsonStr))
+	req.Header.Add("Authorization", bearerToken)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error in response. \n[ERROR] -", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%d status code \n", resp.StatusCode)
+
+	var result *Issue
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	return result, nil
+}
+
+func getBearerToken() string {
+	// This looks like awful code, dubious practice at best
+	// It's done this way so that bearerToken can be returned from the outer scope
+	var bearerToken string
+	var ok bool
+	if bearerToken, ok = os.LookupEnv("BEARER_TOKEN"); !ok {
+		if *issueAction != "read" {
+			log.Println("There is GitHub bearer token in the environment, actions other than read are not possible.")
+			os.Exit(0)
+		}
+	}
+	return bearerToken
 }
